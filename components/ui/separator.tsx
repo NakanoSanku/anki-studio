8da9eb58f4d66a15ad17d386061f2dcb2dfbe5dd
KateTseng <KateTseng.dev@gmail.com>
2026-08-27 08:07:18 +0700

    style(ui): soften separators for pastel design system

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -17,7 +17,7 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
+        "shrink-0 rounded-full bg-black/[0.07] dark:bg-white/[0.11] data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className
       )}
       {...props}
